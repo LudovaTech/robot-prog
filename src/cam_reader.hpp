8ef522a06ed31816e5a0f5a65e4b88993c37fb77
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-06-13 15:28:59 +0200

    add constructors to BallPos, MyGoalPos, EnnemyGoalPos

--- src/cam_reader.hpp
+++ src/cam_reader.hpp
@@ -5,8 +5,16 @@
 
 #include "utilities.hpp"
 
-class BallPos: Vector2 {};
-class MyGoalPos: Vector2 {};
-class EnnemyGoalPos: Vector2 {};
+class BallPos: Vector2 {
+  using Vector2::Vector2;
+};
+
+class MyGoalPos: Vector2 {
+  using Vector2::Vector2;
+};
+
+class EnnemyGoalPos: Vector2 {
+  using Vector2::Vector2;
+};
 
 #endif
