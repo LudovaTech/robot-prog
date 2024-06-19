09ebc2075793875d41b94ffcb3ab1d900f7382d7
DArtagnant <105509558+DArtagnant@users.noreply.github.com>
2024-06-19 20:37:57 +0200

    fix max and min aliases #18

--- test/faking/Arduino.h
+++ test/faking/Arduino.h
@@ -22,9 +22,10 @@
 
 #define sqrt std::sqrt
 #define isDigit std::isdigit
-#define max std::max
-#define min std::min
 typedef uint8_t byte;
+
+using std::max;
+using std::min;
 
 const std::chrono::time_point<std::chrono::steady_clock> _start_time = std::chrono::steady_clock::now();
 unsigned long millis();
