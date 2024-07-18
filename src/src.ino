2e618267cd9496124d393684b019b297a3de8c74
Gandalfph <antonio.guilmot@gmail.com>
2024-07-18 18:03:40 +0200

    changement cote vAT

--- src/src.ino
+++ src/src.ino
@@ -10,14 +10,14 @@
 #include "utilities.hpp"
 
 const FieldProperties fieldProperties = FieldProperties(
-    243,  // fieldLength
-    182,  // fieldWidth
-    12,   // spaceBeforeLineSide
-    60,   // goalWidth
-    115,  // distanceYGoalFromCenter
-    9,    // robotRadius
-    2,    // ballRadius
-    255   // maxDribblerSpeed
+    243, // fieldLength
+    182, // fieldWidth
+    12,  // spaceBeforeLineSide
+    60,  // goalWidth
+    115, // distanceYGoalFromCenter
+    9,   // robotRadius
+    2,   // ballRadius
+    255  // maxDribblerSpeed
 );
 
 const Motors motors(
@@ -37,7 +37,8 @@ uint8_t bigserialbufferlidar[10000];
 const int pinLED = 13;
 const int pinSwitch = 26;
 
-void setup() {
+void setup()
+{
   SerialDebug.begin(115200);
   SerialCam.begin(115200);
   SerialLidar.begin(230400);
@@ -48,7 +49,7 @@ void setup() {
   SerialLidar.addMemoryForRead(&bigserialbufferlidar, sizeof(bigserialbufferlidar));
 
   SerialCam.setTimeout(10);
-  SerialLidar.setTimeout(2);  // !!! 2 !!!
+  SerialLidar.setTimeout(2); // !!! 2 !!!
 
   pinMode(pinLED, OUTPUT);
   pinMode(pinSwitch, INPUT);
@@ -59,27 +60,35 @@ bool ledCounter = true;
 MutableVector2 previousTarget;
 Cache<LidarInfosGlue> cacheLidarInfos(140);
 
-void aloop() {
+void aloop()
+{
   // dribblerKicker.dribble(255);
   // delay(1000);
   // dribblerKicker.dribble(0);
   // delay(1000);
   dribblerKicker.dribble(255);
   dribblerKicker.kick();
-  //delay(1000);
+  // delay(1000);
   dribblerKicker.dribble(255);
   delay(3000);
 }
 
-void loop() {
+Radians lastOrientation = 0;
+bool isFacingMyGoal = true;
+
+void loop()
+{
   unsigned long start_millis = millis();
   log_a(NoteLevel, "src.loop", "***");
 
   // Flash the LED to make sure the code is running correctly
-  if (ledCounter) {
+  if (ledCounter)
+  {
     digitalWrite(pinLED, HIGH);
     ledCounter = false;
-  } else {
+  }
+  else
+  {
     digitalWrite(pinLED, LOW);
     ledCounter = true;
   }
@@ -87,29 +96,39 @@ void loop() {
   // GETTING LIDAR DATA
   LidarInfosGlue lidarInfos;
   Optional<LidarInfosGlue> cacheLidarInfosValue = cacheLidarInfos.cache();
-  if (SerialLidar.available() < 2600 && cacheLidarInfosValue.hasValue()) {
+  if (SerialLidar.available() < 2600 && cacheLidarInfosValue.hasValue())
+  {
     lidarInfos = cacheLidarInfosValue.value();
-  } else {
+  }
+  else
+  {
     lidarInfos = getLidarInfos(fieldProperties, true, false);
     cacheLidarInfos.update(lidarInfos);
   }
 
   String full_log;
-  if (lidarInfos.oLDI.hasValue()) {
+  if (lidarInfos.oLDI.hasValue())
+  {
     full_log += "robot-position: x=" + String(lidarInfos.oLDI.value().coordinates().x()) + " cm, y=" + String(lidarInfos.oLDI.value().coordinates().y()) + " cm, orientation: " + String(lidarInfos.oLDI.value().orientation()) + " rad, ";
-  } else {
+  }
+  else
+  {
     full_log += "robot-position: x= not found, y= not found, orientation: not found, ";
   }
-  if (lidarInfos.oLBI.hasValue()) {
+  if (lidarInfos.oLBI.hasValue())
+  {
     full_log += "nearest-wall-distance=" + String(lidarInfos.oLBI.value().distance(Vector2(0, 0))) + " cm";
-  } else {
+  }
+  else
+  {
     full_log += "nearest-wall-distance= not found";
   }
   log_a(StratLevel, "src.loop", full_log);
 
   Optional<Radians> angleFrontGoalLidar;
   Optional<Radians> angleRearGoalLidar;
-  if (lidarInfos.oLDI.hasValue()) {
+  if (lidarInfos.oLDI.hasValue())
+  {
     angleFrontGoalLidar = Vector2(lidarInfos.oLDI.value().frontGoalCoordinates().x(), lidarInfos.oLDI.value().frontGoalCoordinates().y()).angle();
     angleRearGoalLidar = Vector2(lidarInfos.oLDI.value().rearGoalCoordinates().x(), lidarInfos.oLDI.value().rearGoalCoordinates().y()).angle();
   }
@@ -119,7 +138,8 @@ void loop() {
 
   // GETTING/SENDING BLUE DATA
   BlueInfosGlue blueInfos = getBlueInfos();
-  if (!camInfos.ballPos.hasValue() && blueInfos.ballPos.hasValue()) {
+  if (!camInfos.ballPos.hasValue() && blueInfos.ballPos.hasValue())
+  {
     // camInfos.ballPos = BallPos(
     //     blueInfos.ballPos.value().x(),
     //     blueInfos.ballPos.value().y());
@@ -132,8 +152,10 @@ void loop() {
   float speedReductionRatio = 1;
   float minimumVelocityRatio = 0.5;
   int slowingDownWallDistance = 50;
-  if (lidarInfos.oLBI.hasValue()) {
-    if (lidarInfos.oLBI.value().norm() < slowingDownWallDistance) {
+  if (lidarInfos.oLBI.hasValue())
+  {
+    if (lidarInfos.oLBI.value().norm() < slowingDownWallDistance)
+    {
       speedReductionRatio = minimumVelocityRatio * (lidarInfos.oLBI.value().distance(Vector2(0, 0)) / slowingDownWallDistance + 1);
     }
   }
@@ -146,102 +168,139 @@ void loop() {
       blueInfos.partnerPos,
       blueInfos.ballPos);
 
-  FutureAction currentAction(0, 0, 0, 0);  // va tout de suite être réécris dessus
-  switch (myRole) {
-    case Role::alone:
-      currentAction = chooseStrategyAttacker(
-          fieldProperties,
-          lidarInfos.oLDI,
-          lidarInfos.oLBI,
-          camInfos.ballPos,
-          camInfos.myGoalPos,
-          camInfos.enemyGoalPos,
-          blueInfos.partnerPos);
-      break;
-    case Role::attacker:
-      currentAction = chooseStrategyAttacker(
-          fieldProperties,
-          lidarInfos.oLDI,
-          lidarInfos.oLBI,
-          camInfos.ballPos,
-          camInfos.myGoalPos,
-          camInfos.enemyGoalPos,
-          blueInfos.partnerPos);
-      break;
-    case Role::defender:
-      currentAction = chooseStrategyDefender(
-          fieldProperties,
-          lidarInfos.oLDI,
-          lidarInfos.oLBI,
-          camInfos.ballPos,
-          camInfos.myGoalPos,
-          camInfos.enemyGoalPos,
-          blueInfos.partnerPos);
-      break;
+  FutureAction currentAction(0, 0, 0, 0); // va tout de suite être réécris dessus
+  switch (myRole)
+  {
+  case Role::alone:
+    currentAction = chooseStrategyAttacker(
+        fieldProperties,
+        lidarInfos.oLDI,
+        lidarInfos.oLBI,
+        camInfos.ballPos,
+        camInfos.myGoalPos,
+        camInfos.enemyGoalPos,
+        blueInfos.partnerPos);
+    break;
+  case Role::attacker:
+    currentAction = chooseStrategyAttacker(
+        fieldProperties,
+        lidarInfos.oLDI,
+        lidarInfos.oLBI,
+        camInfos.ballPos,
+        camInfos.myGoalPos,
+        camInfos.enemyGoalPos,
+        blueInfos.partnerPos);
+    break;
+  case Role::defender:
+    currentAction = chooseStrategyDefender(
+        fieldProperties,
+        lidarInfos.oLDI,
+        lidarInfos.oLBI,
+        camInfos.ballPos,
+        camInfos.myGoalPos,
+        camInfos.enemyGoalPos,
+        blueInfos.partnerPos);
+    break;
   }
 
-Radians futureOrientation = currentAction.targetOrientation();
-SerialDebug.println("Orientation strategie : " + String(futureOrientation));
+  Radians futureOrientation = currentAction.targetOrientation();
+  // SerialDebug.println("Orientation strategie : " + String(futureOrientation));
 
-// futureOrientation = Radians(min(PI/4, static_cast<float>(futureOrientation)));
-// futureOrientation = Radians(max(-PI/4, static_cast<float>(futureOrientation)));
-// SerialDebug.println("Orientation recentree : " + String(futureOrientation));
+  // futureOrientation = Radians(min(PI/4, static_cast<float>(futureOrientation)));
+  // futureOrientation = Radians(max(-PI/4, static_cast<float>(futureOrientation)));
+  // SerialDebug.println("Orientation recentree : " + String(futureOrientation));
 
-if (camInfos.enemyGoalPos.hasValue()) {
-    if (camInfos.enemyGoalPos.value().y() < 0) {
-      SerialDebug.println("Se retourne");
-      if (camInfos.enemyGoalPos.value().x() >= 0) {
-        futureOrientation = -PI;
-      } else {
-        futureOrientation = PI;
-      } 
+  // if (camInfos.enemyGoalPos.hasValue()) {
+  //     if (camInfos.enemyGoalPos.value().y() < 0) {
+  //       // SerialDebug.println("Se retourne");
+  //       if (camInfos.enemyGoalPos.value().x() >= 0) {
+  //         futureOrientation = -PI;
+  //       } else {
+  //         futureOrientation = PI;
+  //       }
+  //     }
+  //   } else {
+
+  //     // SerialDebug.println("pas de enemygoal");
+  //   }
+
+  //   if (camInfos.myGoalPos.hasValue()) {
+  //     if (camInfos.myGoalPos.value().y() > 0) {
+  //       // SerialDebug.println("Se retourne");
+  //       if (camInfos.myGoalPos.value().x() >= 0) {
+  //         futureOrientation = PI;
+  //       } else {
+  //         futureOrientation = -PI;
+  //       }
+  //     }
+  //   } else {
+  //     // SerialDebug.println("pas de mygoal");
+  //   }
+  Radians adjestitedOrientation = 0;
+  if (lidarInfos.oLDI.hasValue())
+  {
+    Radians currentOrientation = lidarInfos.oLDI.value().orientation();
+    if (Radians(70) <= lastOrientation && lastOrientation <= Radians(90) && Radians(-90) <= currentOrientation && currentOrientation <= Radians(-70))
+    {
+      SerialDebug.println("change cote");
+      isFacingMyGoal = !isFacingMyGoal;
     }
-  } else {
-    
-    SerialDebug.println("pas de enemygoal");
-  }
-
-  if (camInfos.myGoalPos.hasValue()) {
-    if (camInfos.myGoalPos.value().y() > 0) {
-      SerialDebug.println("Se retourne");
-      if (camInfos.myGoalPos.value().x() >= 0) {
-        futureOrientation = PI;
-      } else {
-        futureOrientation = -PI;
-      } 
+    else if (Radians(-90) <= lastOrientation && lastOrientation <= Radians(-70) && Radians(70) <= currentOrientation && currentOrientation <= Radians(-90))
+    {
+      SerialDebug.println("change cote");
+      isFacingMyGoal = !isFacingMyGoal;
     }
-  } else {
-    SerialDebug.println("pas de mygoal");
+    if (!isFacingMyGoal) {
+      if (currentOrientation < 0) {
+        adjestitedOrientation = -PI/2;
+      } else {
+        adjestitedOrientation = PI/2;
+      }
+    }
   }
+  if (adjestitedOrientation == 0) {
 
   Radians robotOrientationOr0 = 0;
-  if (lidarInfos.oLDI.hasValue()) {
-  robotOrientationOr0 = lidarInfos.oLDI.value().orientation();
+  if (lidarInfos.oLDI.hasValue())
+  {
+    robotOrientationOr0 = lidarInfos.oLDI.value().orientation();
   }
 
-  if (currentAction.changeTarget()) {
-    motors.goTo(currentAction.target(), currentAction.celerity()*speedReductionRatio, futureOrientation - robotOrientationOr0);
+
+  if (currentAction.changeTarget())
+  {
+    motors.goTo(currentAction.target(), currentAction.celerity() * speedReductionRatio, futureOrientation - robotOrientationOr0);
     previousTarget = currentAction.target();
+  }
+  else
+  {
+    motors.goTo(previousTarget.toVector2(), currentAction.celerity() * speedReductionRatio, futureOrientation - robotOrientationOr0);
+  }
+
   } else {
-    motors.goTo(previousTarget.toVector2(), currentAction.celerity()*speedReductionRatio, futureOrientation - robotOrientationOr0);
+    motors.goTo(currentAction.target(), currentAction.celerity() * speedReductionRatio, adjestitedOrientation);
   }
 
   String full_log2;
-  if (currentAction.changeTarget()) {
+  if (currentAction.changeTarget())
+  {
     full_log2 += "Target : " + currentAction.target().toString() + " ";
-  } else {
+  }
+  else
+  {
     full_log2 += "Target : Unchanged (" + previousTarget.toVector2().toString() + ") ";
   }
   full_log2 += "Vitesse : " + String(currentAction.celerity() * speedReductionRatio) + " Rotation : " + String(futureOrientation);
   log_a(InfoLevel, "src.loop", full_log2);
-  
-  //dribblerKicker.dribble(currentAction.celerityDribbler());
-  if (currentAction.activeKicker()) {
-    delay(100);  // A changer
+
+  // dribblerKicker.dribble(currentAction.celerityDribbler());
+  if (currentAction.activeKicker())
+  {
+    delay(100); // A changer
     dribblerKicker.kick();
   }
 
-  dribblerKicker.dribble(255);  // TODO Rustine
+  dribblerKicker.dribble(255); // TODO Rustine
   // SerialDebug.println("********************************* " + String(currentAction.celerityDribbler()));
 
   unsigned long elapsed = millis() - start_millis;
